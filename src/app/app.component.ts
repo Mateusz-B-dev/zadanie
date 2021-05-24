6189ccce9256255b3c351a9f82b05e3b2e118cc0
Mateusz-B-dev <mati91@onet.eu>
2021-05-24 18:51:50 +0200

    zadanie-rekrutacyjne

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  item = 50;
+
+  formContact = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    phone: new FormControl('', Validators.required),
+    street: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    code: new FormControl('', Validators.required),
+    rodo: new FormControl('', Validators.requiredTrue)
+
+  });
+
   title = 'zadanie';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+
+  sendMoney(item): void {
+    switch (item.id) {
+      case 'btnradio1':
+        this.item = 50;
+        break;
+      case 'btnradio2':
+        this.item = 70;
+        break;
+      case 'btnradio3':
+        this.item = 100;
+        break;
+    }
+    console.log(this.formContact.value);
+    console.log(this.item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    MDBBootstrapModule.forRoot(),
+    ReactiveFormsModule,
+    MatButtonModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
